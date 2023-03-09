ed4272b045b1a61063482c079d276ace6aa7349c
KubaOw <kuba.owczarek@icloud.com>
2023-03-09 12:14:35 +0100

    Change of BmiDeterminatorTests
    Changed the test by added second parameter to check bmi value and its classification

--- Tests/Tests.Tests/BmiDeterminatorTests.cs
+++ Tests/Tests.Tests/BmiDeterminatorTests.cs
@@ -5,18 +5,24 @@ namespace Tests.Tests
     public class BmiDeterminatorTests
     {
         [Theory]
-        [InlineData(10.0)]
-        [InlineData(17.0)]
-        [InlineData(18.0)]
-        [InlineData(8.0)]
-        public void DeterminateBmi_ForBmiBelow18_5_ReturnsUnderweightClassification(double bmi)
+        [InlineData(10.0, BmiClassification.Underweight)]
+        [InlineData(18.0, BmiClassification.Underweight)]
+        [InlineData(8.0, BmiClassification.Underweight)]
+        [InlineData(19.0, BmiClassification.Normal)]
+        [InlineData(21.0, BmiClassification.Normal)]
+        [InlineData(24.8, BmiClassification.Normal)]
+        [InlineData(25.8, BmiClassification.Overweight)]
+        [InlineData(28.8, BmiClassification.Overweight)]
+        [InlineData(30.8, BmiClassification.Obesity)]
+        [InlineData(35.9, BmiClassification.ExtremeObesity)]
+        public void DeterminateBmi_ForGivenBmi_ReturnsCorrectClassification(double bmi, BmiClassification classification)
         {
             //arrange
             BmiDeterminator bmiDeterminator = new BmiDeterminator();
             //act
             BmiClassification result = bmiDeterminator.DeterminateBmi(bmi);
             //assert
-            Assert.Equal(BmiClassification.Underweight, result);
+            Assert.Equal(classification, result);
         }
     }
 }
