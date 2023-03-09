49c7d295d15bd19e5eef239aa8efd5f59b93d423
KubaOw <kuba.owczarek@icloud.com>
2023-03-09 12:18:33 +0100

    Calculator's tests
    Added two new tests, which checked if Imperial and Metric calculator work correct

---
+++ Tests/Tests.Tests/ImperialBmiCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Calculator;
+
+namespace Tests.Tests
+{
+    public class ImperialBmiCalculatorTest
+    {
+        [Theory]
+        [InlineData(100, 170, 2.43)]
+        [InlineData(77, 160, 2.11)]
+        [InlineData(90, 190, 1.75)]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+            //arrange 
+            ImperialBmiCalculator calculator = new ImperialBmiCalculator();
+            //act
+            double result = calculator.CalculateBmi(weight, height);
+            //assert
+            Assert.Equal(bmiResult, result);
+        }
+    }
+}
---
+++ Tests/Tests.Tests/MetricBmiCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Calculator;
+
+namespace Tests.Tests
+{
+    public class MetricBmiCalculatorTest
+    {
+        [Theory]
+        [InlineData(100, 170, 34.6)]
+        [InlineData(77, 160, 30.08)]
+        [InlineData(90, 190, 24.93)]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+            //arrange 
+            MetricBmiCalculator calculator = new MetricBmiCalculator();
+            //act
+            double result = calculator.CalculateBmi(weight, height);
+            //assert
+            Assert.Equal(bmiResult, result);
+        }
+    }
+}
